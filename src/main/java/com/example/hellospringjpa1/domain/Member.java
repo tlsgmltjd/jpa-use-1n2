25b406ecbc136edc4a5a3fad06df8e2d53e4f10d
신희성 <vvwv3092@gmail.com>
2023-12-27 08:36:04 +0900

    Update casecade, relation mapping method

--- src/main/java/com/example/hellospringjpa1/domain/Member.java
+++ src/main/java/com/example/hellospringjpa1/domain/Member.java
@@ -20,4 +20,6 @@ public class Member {
 
     @OneToMany(mappedBy = "member") // 하나의 회원이 여러개의 Order를 갖는다.
     private List<Order> orders = new ArrayList<>();
+    // 컬렉션은 필드에서 바로 초기화 하는 것이 안전하다
+    // null 문제에서 안전함, 하이버네이트는 영속화 할때 컬렉션을 감싸서 하이버네이트가 제공하는 내장 컬렉션으로 변경한다. 컬력션을 변경한다면 문제가 생김
 }
--- src/main/java/com/example/hellospringjpa1/domain/Order.java
+++ src/main/java/com/example/hellospringjpa1/domain/Order.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
@@ -21,10 +22,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +33,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // ====== 연관관계 편의 메서드 ======
+    // 핵심적으로 컨트롤 하는 곳에 있는 것이 좋음
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this); // 양방향 연관관계 한번에 걸기
+    }
+
+    public void setOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
