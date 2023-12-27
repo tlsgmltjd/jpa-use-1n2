c1c5c3920677517aff98bafabc07f99ec9f6720b
신희성 <vvwv3092@gmail.com>
2023-12-27 10:14:19 +0900

    Add Member Repository

---
+++ src/main/java/com/example/hellospringjpa1/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package com.example.hellospringjpa1.repository;
+
+import com.example.hellospringjpa1.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceUnit;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+//    @PersistenceUnit
+//    private EntityManagerFactory entityManagerFactory;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("SELECT m FROM Member m WHERE m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
