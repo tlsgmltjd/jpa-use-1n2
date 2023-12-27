ad63139cc1414f060c677e514b587c3c7c6a1fc3
신희성 <vvwv3092@gmail.com>
2023-12-27 20:20:37 +0900

    Add Item entity

--- src/main/java/com/example/hellospringjpa1/domain/item/Item.java
+++ src/main/java/com/example/hellospringjpa1/domain/item/Item.java
@@ -1,5 +1,6 @@
 package com.example.hellospringjpa1.domain.item;
 
+import com.example.hellospringjpa1.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -23,4 +24,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+    // DDD 엔티티가 해결할 수 있는 작업은 엔티티에 넣는게 좋다. 객체 지향적임
+    // 데이터를 가지고 있는 클래스에서 비즈니스로직을 가져가는게 응집도가 있다.
+
+    /**
+     * stock 증가
+     * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
