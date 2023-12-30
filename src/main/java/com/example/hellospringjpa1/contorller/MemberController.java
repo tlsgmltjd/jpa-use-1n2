455fc78425d3d07592e50ee6976384d4a1ee5c0f
신희성 <vvwv3092@gmail.com>
2023-12-30 23:46:50 +0900

    Add createMember view

---
+++ src/main/java/com/example/hellospringjpa1/contorller/MemberController.java
@@ -0,0 +1,20 @@
+package com.example.hellospringjpa1.contorller;
+
+import com.example.hellospringjpa1.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+}
