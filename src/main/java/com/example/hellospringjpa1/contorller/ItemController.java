5eec6bd3873e17c7734ec9ade4297aca12648cc4
신희성 <vvwv3092@gmail.com>
2024-01-02 20:00:54 +0900

    Add Item create controller, view

---
+++ src/main/java/com/example/hellospringjpa1/contorller/ItemController.java
@@ -0,0 +1,36 @@
+package com.example.hellospringjpa1.contorller;
+
+import com.example.hellospringjpa1.domain.item.Book;
+import com.example.hellospringjpa1.service.ITemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ITemService iTemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    private String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setPrice(form.getPrice());
+        book.setAuthor(form.getAuthor());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setIsbn(form.getIsbn());
+
+        iTemService.saveItem(book);
+        return "redirect:/";
+    }
+}
